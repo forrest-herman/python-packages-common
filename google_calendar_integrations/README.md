fddb88d2c28abbecea65fe4cc40d2fd58aff9894
forrest-herman <forrestherman13@gmail.com>
2022-07-12 15:53:54 -0400

    prepare package

--- google_calendar_integrations/README.md
+++ google_calendar_integrations/README.md
@@ -1,1 +1,6 @@
 # Google Calendar Methods
+
+### This package gives access to the Google Calendar API
+- Easily get all events from all calendars
+- Utilities:
+  - Easily sort through events and include/exclude specific calendars
--- google_calendar_integrations/setup.py
+++ google_calendar_integrations/setup.py
@@ -8,10 +8,10 @@ setup(
     author_email='forrestherman13@gmail.com',
     # list folders, not files
     packages=['google_calendar_integrations', 'google_calendar_integrations.tests'],
-    scripts=['bin/script1', 'bin/script2'],
+    # scripts=['bin/script1', 'bin/script2'],
     # url='http://pypi.python.org/pypi/PackageName/',
     license='LICENSE.txt',
-    long_description=open('README.txt').read(),
+    # long_description=open('README.txt').read(),
     install_requires=[
         "google-auth-oauthlib",
         "google-api-python-client",
